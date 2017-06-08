20f18e919a1fee28737bcef037ae4a528e30d6ee
Alex Kofke <akofke@gmail.com>
2017-06-08 09:39:05 -0400

    open xdmod link

--- xdmod_open.js
+++ xdmod_open.js
@@ -4,5 +4,7 @@ plot.on('plotly_click', function(data) {
     var pointNum = point['pointNumber'];
     var text = point['data']['text'][pointNum];
     var ids = text.split(",");
-    console.log(ids)
+    console.log(ids);
+    var url = `https://metrics-dev.ccr.buffalo.edu/#job_viewer?action=search&realm=SUPREMM&resource_id=${ids[0]}&local_job_id=${ids[1]}`;
+    window.open(url)
 });
